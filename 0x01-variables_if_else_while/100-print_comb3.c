13637e470e39a25d3173b1d1c8c2e33d97018ae2
egwuenu precious <egwuenuprecious7@gmail.com>
2022-09-09 16:45:24 -0700

    dddd

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -17,12 +17,14 @@ while (c < 10)
 {
 	putchar('0' + d);
 	putchar('0' + c);
-if (c + d != 18)
+if (c + d != 17)
 {
 	putchar(',');
 	putchar(' ');
-}c++;
-}d++;
+}
+c++;
+}
+d++;
 }
 putchar('\n');
 return (0);
