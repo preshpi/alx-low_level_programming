a2c05a52a36d3f569af8ce7016db12db7d1fd627
egwuenu precious <egwuenuprecious7@gmail.com>
2022-09-09 15:13:35 -0700

    4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -5,7 +5,8 @@
 * Desctiption: using the main function
 * this program prints "Programming is positive, zero, or negative
 * Return: 0
-*/int main(void)
+*/
+int main(void)
 {
 char ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
