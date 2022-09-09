a6f86cdfb6a8dfc702e70a0ae7899bf6ee42b604
egwuenu precious <egwuenuprecious7@gmail.com>
2022-09-09 11:31:00 -0700

    pos and neg

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -10,5 +10,11 @@ int n;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 /* your code goes ther */
+if ( n > 0 )
+	printf("is positive");
+if ( n = 0 )
+	printf("is Zero");
+if ( n < 0 )
+	printf("is negative");
 return (0);
 }
