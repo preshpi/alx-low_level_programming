a492ac5ebd5f3f6cf134ab87295d33fdc1ba03e7
egwuenu precious <egwuenuprecious7@gmail.com>
2022-09-09 18:11:30 -0700

    s

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 /**
-* main - print if the number is posutuve, zero, or negative
+* main - print if the number is positive, zero, or negative
 *
-* Description: using the main description
-* this program prints "Programming is positive, zero or negative
+* Description: using the main function
+* this program prints "Programming is positive, zero, or negative
 * Return: 0
 */
 int main(void)
@@ -15,7 +15,7 @@ int f = 0;
 while (f < 10)
 {
 e = 0;
-while (e < 10)
+while ( e < 10)
 {
 d = 0;
 while (d < 10)
@@ -23,20 +23,20 @@ while (d < 10)
 c = 0;
 while (c < 10)
 {
-if (!(f == c && e == d))
-{
-	putchar('0' + f);
-	putchar('0' + e);
-	putchar(' ');
-	putchar('0' + d);
-	putchar('0' + c);
-if (!(f + e == 18 && c + d == 17 && d == 9))
-{
-	putchar(',');
-	putchar(' ');
-}
-}
-c++;
+	if (!(f == c && e == d))
+	{
+		putchar('0' + f);
+		putchar('0' + e);
+		putchar(' ');
+		putchar('0' + d);
+		putchar('0' + c);
+		if (!(f + e == 18 && c + d == 17 && d == 9))
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	c++;
 }
 d++;
 }
