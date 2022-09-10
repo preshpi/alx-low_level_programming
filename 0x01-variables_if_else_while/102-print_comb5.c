d08ff24298f7810665303e5ba0be8e2f203d28c8
egwuenu precious <egwuenuprecious7@gmail.com>
2022-09-10 01:58:06 -0700

    s

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -2,28 +2,27 @@
 /**
 * main - print if the number is positive, zero, or negative
 *
-* Description: using the main function
-* this program prints "Programming is positive, zero, or negative
+* Description: using the main descripition
+* this number prints "Programming is positive, zero, or negative
 * Return: 0
 */
 int main(void)
 {
 int p, q;
-for (p = 0; p <= 98; p++)
+for (p = 0; p <= 98; q++)
 {
-	for (q = p + 1; q <= 99; q++)
+	for (q = p +1; q <= 99; q++)
 	{
-	putchar((p / 10) + '0');
-	putchar((p % 10) + '0');
-	putchar(' ');
-	putchar((q / 10) + '0');
-	putchar((q % 10) + '0');
-	
-	if (p == 98 && q == 99)	
-	{
-		putchar(',');
+		putchar((p / 10) + '0');
+		putchar((p % 10) + '0');
 		putchar(' ');
-	}
+		putchar((q / 10) + '0');
+		putchar((q % 10) + '0');
+		if(p != 98 || q != 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
 	}
 }
 putchar('\n');
