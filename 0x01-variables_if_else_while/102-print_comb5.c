def3bd18e3a08479c2a0ce19ee59d21905b03957
egwuenu precious <egwuenuprecious7@gmail.com>
2022-09-10 08:09:07 -0700

    dd

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -11,14 +11,14 @@ int main(void)
 int p, q;
 for (p = 0; p <= 98; q++)
 {
-	for (q = p +1; q <= 99; q++)
+	for (q = p + 1; q <= 99; q++)
 	{
 		putchar((p / 10) + '0');
 		putchar((p % 10) + '0');
 		putchar(' ');
 		putchar((q / 10) + '0');
 		putchar((q % 10) + '0');
-		if(p != 98 || q != 99)
+		if (p != 98 || q != 99)
 		{
 			putchar(',');
 			putchar(' ');
