fdf557903f80c1c5e37cebf9a3468b81118025fa
egwuenu precious <egwuenuprecious7@gmail.com>
2022-09-13 11:06:33 -0700

    ddd

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -8,9 +8,9 @@ void times_table(void)
 {
 	int a, b, product;
 
-	for (a = 0; a <=9; a++)
+	for (a = 0; a <= 9; a++)
 	{
-		for (b =0; b <=9; b++)
+		for (b = 0; b <= 9; b++)
 		{
 			product = a * b;
 			if (b != 0)
@@ -24,7 +24,7 @@ void times_table(void)
 			}
 			else if (product >= 10)
 			{
-				_putchar((product /10) + '0');
+				_putchar((product / 10) + '0');
 				_putchar((product % 10) + '0');
 			}
 			else if ((product < 10) && (b != 0))
