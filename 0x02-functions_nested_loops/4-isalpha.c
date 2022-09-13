ef51574bafaed135eb7f0695517a1fe899391de2
egwuenu precious <egwuenuprecious7@gmail.com>
2022-09-13 10:04:19 -0700

    jk

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isalpha - Print for letters
+ * @c: is the int that will use for argument
+ * Return: 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c>= 'A' && c <= 'z'))
+	{
+		return (1);
+	}
+	else
+		return (0);
+}
