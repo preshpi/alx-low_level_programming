e37328ef096a51532c460c12d13f60561a537caf
egwuenu precious <egwuenuprecious7@gmail.com>
2022-09-13 09:51:39 -0700

    betty

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
  * _islower - Print a function for lowercase
- *
+ * @c: is the int that will use for the argument of the function
  * Description: int _islower(int c)
  *
  * Return: 0
