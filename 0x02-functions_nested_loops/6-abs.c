13c9bba8823d9a50839f97ea9b1d866a70d150bf
egwuenu precious <egwuenuprecious7@gmail.com>
2022-09-13 11:32:47 -0700

    sdd

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * _abs - compute the absolutes value of an integer
+ *
+ * @n: number to be tested
+ * Return: 0
+ */
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (-n);
+	}
+	else
+	{
+		return (n);
+	}
+	return (0);
+
+}
