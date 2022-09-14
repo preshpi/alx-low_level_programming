4cfe3259d0fce2fca50fe3e9be0990605d5ecf46
egwuenu precious <egwuenuprecious7@gmail.com>
2022-09-14 12:57:02 -0700

    loops

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -4,6 +4,7 @@
  * main - cause an infinite loop
  * Return: 0
  */
+
 int main(void)
 {
 	int i;
@@ -12,14 +13,12 @@ int main(void)
 
 	i = 0;
 /*
- * while (i <10)
+ * while (i < 10)
  * {
  * putchar(i);
  * }
  */
+	printf("Infinite loop avoided! \\o/\n");
 
-
-printf("Infinite loop avoided! \\o/\n");
-
-return (0);
+	return (0);
 }
