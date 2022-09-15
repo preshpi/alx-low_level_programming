40099d4be0cf9775f1e4048952419a33386e6efe
egwuenu precious <egwuenuprecious7@gmail.com>
2022-09-15 05:50:58 -0700

    numbers

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - entry point
+ * Description: prints the numbers with _putchar
+ *
+ * Return: void
+ */
+
+void print_numbers(void)
+{
+	int n;
+	for (n = '0'; n <= '9'; n++)
+	{
+		_putchar(n);
+	}
+	_putchar("\n");
+}
