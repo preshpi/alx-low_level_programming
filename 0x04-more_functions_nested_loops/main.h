08b176c4ab42be6babb248d9e222b3183ca006af
egwuenu precious <egwuenuprecious7@gmail.com>
2022-09-15 07:47:22 -0700

    dd

--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -19,5 +19,7 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_triangle(int size);
 void print_number(int n);
-
+int isPrime(long int n);
+long int biggestFactor(long int a);
+int numDigits(int n);
 #endif /* MAIN_H */
