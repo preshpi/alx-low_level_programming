9d5a578a074f9701bf15c5b731f718436aaff5ba
egwuenu precious <egwuenuprecious7@gmail.com>
2022-09-19 05:39:52 -0700

    swap me

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -7,5 +7,13 @@
  */
 void swap_int(int *a, int *b)
 {
-	swap_int(a, b);
+	int a;
+	int b;
+
+	a = 98;
+	b = 42;
+	_putchar('a=%d, b=%d\n', a, b);
+	swap_int(&a, &b);
+	_putchar('a=%d, b=%d\n', a, b);
+return (0);
 }
